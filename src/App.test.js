4372a8e86760794c2779fbeb00a85324f5ae8b0b
jazeee <jazeee@gmail.com>
2017-07-07 11:15:43 -0700

    Add snapshot tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {shallow, mount, render} from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('renders correctly', () => {
+  const wrapper = shallow(
+    <App/>
+  );
+
+  expect(toJson(wrapper)).toMatchSnapshot();
 });
